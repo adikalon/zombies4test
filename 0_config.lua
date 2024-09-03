170e0bd2e1d93692adc614f27f91f248d09ce384
adikalon <webadev@yandex.ru>
2024-09-03 19:34:04 +0300

    Walking Zombie

--- 0_config.lua
+++ 0_config.lua
@@ -101,8 +101,19 @@ zombies4test.spawns = {
 		min_height = zombies4test.height.min,
 		max_height = -25,
 	},
-	survivorzombie = {
+	tankzombie = {
 		name = "zombies4test:tankzombie",
+		nodes = {"group:cracky", "group:crumbly"},
+		neighbors = {"air"},
+		max_light = 7,
+		interval = 30,
+		chance = 20000,
+		active_object_count = 1,
+		min_height = zombies4test.height.min,
+		max_height = zombies4test.height.max,
+	},
+	walkingzombie = {
+		name = "zombies4test:walkingzombie",
 		nodes = {"group:cracky", "group:crumbly"},
 		neighbors = {"air"},
 		max_light = 7,
