1a19ddf41918916de8c378973854f21acf9a094f
adikalon <webadev@yandex.ru>
2024-09-03 18:07:59 +0300

    Transferring files

---
+++ 0_config.lua
@@ -0,0 +1,43 @@
+-- HEIGHTS
+zombies4test.height = {
+	min = -31000,
+	max = 31000,
+}
+
+if minetest.get_modpath("nether") then
+	zombies4test.height.min = nether.DEPTH_CEILING
+end
+
+if minetest.get_modpath("cloudlands") then
+	zombies4test.height.max = tonumber(minetest.settings:get("cloudlands_altitude")) or 200
+end
+
+if minetest.get_mapgen_setting("mg_name") == "v7" then
+	local mgv7_spflags = minetest.get_mapgen_setting("mgv7_spflags")
+	local captures_float = string.match(mgv7_spflags, "floatlands")
+	local captures_nofloat = string.match(mgv7_spflags, "nofloatlands")
+
+	if not captures_nofloat and captures_float and zombies4test.height.max > 1280 then
+		zombies4test.height.max = 1280
+	end
+end
+
+-- SPAWN NODES
+zombies4test.spawn_nodes = {
+	zombie = {"group:cracky", "group:crumbly"},
+}
+
+-- SPAWNS
+zombies4test.spawns = {
+	zombie = {
+		name = "mobs_mc:zombie",
+		nodes = zombies4test.spawn_nodes.zombie,
+		neighbors = {"air"},
+		max_light = 7,
+		interval = 30,
+		chance = 20000,
+		active_object_count = 1,
+		min_height = zombies4test.height.min,
+		max_height = zombies4test.height.max,
+	},
+}
---
+++ init.lua
@@ -0,0 +1,32 @@
+local path = minetest.get_modpath(minetest.get_current_modname())
+
+zombies4test = {}
+
+zombies4test.S = minetest.get_translator(minetest.get_current_modname())
+
+zombies4test.custom_spawn = false
+local input = io.open(path .. "/spawn.lua", "r")
+
+if input then
+	zombies4test.custom_spawn = true
+	input:close()
+	input = nil
+end
+
+-- CONFIG
+dofile(path .. "/0_config.lua")
+
+-- ZOMBIES
+dofile(path .. "/crawlerzombie.lua")
+dofile(path .. "/doctorzombie.lua")
+dofile(path .. "/lumberjackzombie.lua")
+dofile(path .. "/minerzombie.lua")
+dofile(path .. "/runner.lua")
+dofile(path .. "/spitterzombie.lua")
+dofile(path .. "/survivorzombie.lua")
+dofile(path .. "/tankzombie.lua")
+dofile(path .. "/walkingzombie.lua")
+
+if zombies4test.custom_spawn then
+	dofile(path .. "/spawn.lua")
+end
