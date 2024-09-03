8ee7751aef44838cea6794e59decd8ad3d7cdbe7
adikalon <webadev@yandex.ru>
2024-09-03 18:55:48 +0300

    Doctor Zombie

--- 0_config.lua
+++ 0_config.lua
@@ -35,4 +35,15 @@ zombies4test.spawns = {
 		min_height = zombies4test.height.min,
 		max_height = zombies4test.height.max,
 	},
+	doctorzombie = {
+		name = "zombies4test:doctorzombie",
+		nodes = {"group:cracky", "group:crumbly"},
+		neighbors = {"air"},
+		max_light = 7,
+		interval = 30,
+		chance = 20000,
+		active_object_count = 1,
+		min_height = zombies4test.height.min,
+		max_height = zombies4test.height.max,
+	},
 }
